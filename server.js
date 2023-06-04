75bfb85b9dca490582dcebbe02b3679f6d26c4e8
delfii03 <delfina.andreuccetti@gmail.com>
2023-06-04 10:13:22 -0300

    .

--- server.js
+++ server.js
@@ -1,48 +1,13 @@
-const session = require('express-session');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const express = require('express');
+const app = express();
 
-class AuthorizationGoogle {
-  constructor(app) {
-    const GOOGLE_CLIENT_ID = "782947744959-m58n1q0403ampr46n00jtqer4ju28h6e.apps.googleusercontent.com";
-    const GOOGLE_CLIENT_SECRET = "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";
+app.use(express.static('public'));
 
-    // Configuración del middleware de sesión
-    app.use(session({
-      secret: "secret",
-      resave: false,
-      saveUninitialized: true,
-    }));
+app.get('/', function (req, res) {
+  res.send('Main page!');
+});
 
-    app.use(passport.initialize());
-    app.use(passport.session());
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
+});
 
-    passport.use(new GoogleStrategy({
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
-      passReqToCallback: true
-    }, this._verify.bind(this))); // Corregido: Agregar bind(this) para mantener el contexto de la clase
-
-    passport.serializeUser((user, done) => done(null, user));
-    passport.deserializeUser((user, done) => done(null, user));
-  }
-
-  _verify(request, accessToken, refreshToken, profile, done) {
-    console.log(profile);
-    return done(null, profile);
-  }
-
-  checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/login");
-  }
-}
-
-module.exports = AuthorizationGoogle;
-
-app.post('/login', (req, res) => {
-    // Aquí se maneja la solicitud de inicio de sesión del cliente
-  });
